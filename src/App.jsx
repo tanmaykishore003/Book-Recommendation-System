d706361c30e555d4f444ab47344459442ad9936f
tanmaykishore003 <tanmaykishore003@gmail.com>
2024-04-06 19:17:17 +0530

    Initial Setup

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <BrowserRouter>
+          <Routes>
+              
+          </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
